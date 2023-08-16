cf278c3cafcb9f6e94a9861ca2a5d3e75f8829a9
Julio Donado <jdonado@unibol.com.co>
2023-08-16 16:37:16 -0500

    Added dotenv dependency and configured it in app.js.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const hbs = require('hbs');
+require('dotenv').config()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 // HANDLEBARS
 app.set('view engine', 'hbs')
